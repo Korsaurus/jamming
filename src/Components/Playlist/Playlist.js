1a105b353dc4ea0c49570284461f218066043595
Korsaurus <grkorsos@gmail.com>
2021-09-22 13:16:21 -0700

    a

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -4,12 +4,26 @@ import "./Playlist.css";
 import TrackList from "../TrackList/Tracklist";
 
 class Playlist extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.handleNameChange = this.handleNameChange.bind(this)
+    }
+
+        //Targeting the onNameChange attribute of the playlist component in app.js
+        handleNameChange(event) {
+            this.props.onNameChange(event.target.value)
+        }
+
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={"New Playlist"}/>
-                <TrackList tracks={this.props.playlistTracks} />
-                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+                <input defaultValue={"New Playlist"}
+                    onChange={this.handleNameChange} />
+                <TrackList tracks={this.props.playlistTracks} 
+                    onRemove={this.props.onRemove}
+                    isRemoval={true} />
+                <button className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</button>
             </div>
         )
     }
--- src/Components/TrackList/Tracklist.js
+++ src/Components/TrackList/Tracklist.js
@@ -7,12 +7,15 @@ import Track from "../Track/Track"
 class TrackList extends React.Component {
     render() {
         return (
-            <div class="TrackList">
+            <div className="TrackList">
                 {
                     // passes down songname etc to each track object
                     this.props.tracks.map(track => {
                         return <Track track={track}
-                            key={track.id} />
+                            key={track.id}
+                            onAdd={this.props.onAdd}
+                            onRemove={this.props.onRemove}
+                            isRemoval={this.props.isRemoval} />
                     })
                 }
             </div>
