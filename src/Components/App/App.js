e030a74148bab814a2423a69edcff333c7990716
Korsaurus <grkorsos@gmail.com>
2021-09-21 17:59:00 -0700

    Hardcoded search results and playlists

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,33 @@
 import React from "react";
 import './App.css';
 
+import  SearchBar from "../SearchBar/SearchBar"
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
+
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+	this.state = {
+		searchResults: [{ name: "name1", artist: "artist1", album: "album1", id: 1 }, 
+		{ name: "name2", artist: "artist2", album: "album2", id: 2 }],
+		playlistName: "My Playlist",
+		playlistTracks: [{ name: "playlistName1", artist: "playlistArtist1", album: "playlistAlbum1", id: 3 },
+		{ name: "playlistName2", artist: "playlistArtist2", album: "playlistAlbum2", id: 4 }]
+	}
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-            {/* <SearchBar /> */}
+            <SearchBar />
           <div className="App-playlist">
-            {/* <SearchResults />
-            <Playlist /> */}
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} /> 
           </div>
         </div>
       </div>
